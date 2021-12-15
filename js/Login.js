e801e46713ad58bfc416ad226df9429235fd669a
Daniel Castro <daniel.castro.8827@gmail.com>
2021-12-14 19:38:52 -0600

    banner y registro

--- js/Login.js
+++ js/Login.js
@@ -30,3 +30,78 @@ function Login() {
     .then(() => { tel.value = ""; con.value = "" })
     .catch(error => console.error("No se pudo realizar la accion", error));
 }
+
+function AbrirRegistrar() {
+  $("#RegistrarForm").fadeIn().prop("hidden", false);
+  $("a").prop("hidden", true);
+}
+
+function CerrarRegistrar() {
+  $("a").prop("hidden", false);
+  $("#RegistrarForm").slideUp();
+}
+
+function AbrirContrasena() {
+  $("#FormContrasena").fadeIn().prop("hidden", false);
+}
+
+function CerrarContrasena(){
+  $("#FormContrasena").slideUp().prop("hidden", true);
+  $("#InputContrasena").slideUp().prop("hidden", true);
+}
+
+function VerificarTelefono() {
+  let telefono = $("#TelefonoContrasena").val();
+  $.get("https://desarrollowebapi.azurewebsites.net/api/Usuario/Get/" + telefono, function (respuesta) {
+      //console.log(respuesta.isCompletedSuccessfully);
+      if (respuesta.isCompletedSuccessfully === true) {
+          $("#InputContrasena").fadeIn().prop("hidden", false);
+          $("#ContrasenaVerificada").val(respuesta.result.contrasena);
+      } else {
+          alert("no se encuentra registrado en nuestros usuarios");
+      }
+  });
+}
+
+function ShowPassword() {
+  let contrasena = document.getElementById("ContrasenaVerificada");
+  if (contrasena.type === "password") {
+      contrasena.type = "text";
+  } else {
+      contrasena.type = "password";
+  }
+}
+
+function GuardarCliente() {
+  const url = "https://desarrollowebapi.azurewebsites.net/api/Usuario/";
+  let nombre = $("#NombreUsuario").val();
+  let apellido = $("#ApellidoUsuario").val();
+  let contrasena = $("#ContrasenaUsuario").val();
+  let telefono = $("#TelefonoUsuario").val();
+  let correo = $("#CorreoUsuario").val();
+  let direccion = $("#DireccionUsuario").val();
+
+  const item = {
+      nombre: nombre,
+      apellido: apellido,
+      contrasena: contrasena,
+      telefono: telefono,
+      correo: correo,
+      direccion: direccion
+  };
+
+  fetch(url, {
+      method: "POST",
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(item)
+  })
+      .then(respuesta => respuesta.json())
+      .then(() => {
+          alert("Usuario Registrado!");
+          CerrarRegistrar();
+      })
+      .catch(error => console.error("no se pudo registrar el usuario", error));
+}
